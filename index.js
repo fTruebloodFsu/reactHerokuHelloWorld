9aead9d766ac2637bc2482b8575b494736c6f936
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-02-16 14:42:14 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import resume from './FTResume.PNG';
+import resumePDF from './Fletcher_Trueblood_Resume.pdf';
+
+var element = React.createElement('img', {className: 'myResume'}, 
+                                 {src: "FTResume.png"});
+
+function DisplayResume(){
+    return(
+        <div className="resume">
+            <iframe title="myResume" src={resumePDF} width="80%" height="1000"></iframe>
+        </div>
+    );
+}
+
+ReactDOM.render(DisplayResume(), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
