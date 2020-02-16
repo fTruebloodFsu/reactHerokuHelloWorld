9a53869491e83133c8a66aaeaedd096f61fca237
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-02-16 14:46:40 -0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import resume from './FTResume.PNG';
+import resumePDF from './Fletcher_Trueblood_Resume.pdf';
 
-var element = React.createElement('h1', {className: 'greeting'}, 'Hello World!');
+var element = React.createElement('img', {className: 'myResume'}, 
+                                 {src: "FTResume.png"});
 
-ReactDOM.render(element, document.getElementById('root'));
+function DisplayResume(){
+    return(
+        <div className="resume">
+            <iframe title="myResume" src={resumePDF} width="80%" height="1000"></iframe>
+        </div>
+    );
+}
+
+ReactDOM.render(DisplayResume(), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
